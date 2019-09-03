682fb6ec1fe0c09501539d8e3a0098886339419f
camille villard <hello@camillevillard.com>
2019-09-03 19:31:29 -0400

    added styles for desktop

--- app/javascript/plugins/animations.js
+++ app/javascript/plugins/animations.js
@@ -1,6 +1,19 @@
 const sliderWine = document.querySelector('#slide-up-animation')
+const showMoreBtn = document.querySelector('p.show-more')
 
-sliderWine.addEventListener('click', e => {
-  console.log('clicked')
-  sliderWine.classList.toggle('show-cards')
-})
+
+if (window.innerWidth < 1024) {
+  // displays the list of wine - mobile only
+  sliderWine.addEventListener('click', e => {
+    console.log('clicked')
+    sliderWine.classList.toggle('show-cards')
+  })
+
+  // changes the content of the div
+  showMoreBtn.innerHTML = "voir la liste des vins"
+
+} else {
+  // changes the content of the div
+  showMoreBtn.innerHTML = "liste des vins"
+}
+
