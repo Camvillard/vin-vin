beebb04508990b1bce3b63c15aec9072cc7ad7c5
camille villard <hello@camillevillard.com>
2019-09-06 15:48:58 -0400

    added some style  to the homepage

--- app/javascript/plugins/animations.js
+++ app/javascript/plugins/animations.js
@@ -5,11 +5,19 @@ const showInfosBtn = document.querySelector('#show-infos')
 const infosContainer = document.querySelector('#infos-container')
 
 
+let counter = 0
 if (window.innerWidth < 1024) {
   // displays the list of wine - mobile only
   sliderWine.addEventListener('click', e => {
     console.log('clicked')
+    counter = counter + 1
+    console.log(counter)
     sliderWine.classList.toggle('show-cards')
+    if (counter % 2 == 0) {
+      showMoreBtn.innerHTML = "voir la liste des vins"
+    } else {
+      showMoreBtn.innerHTML = "fermer"
+    }
   })
 
   // changes the content of the div
