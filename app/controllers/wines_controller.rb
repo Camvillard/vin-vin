212996769dc88ab3c94c4ffaba676c97b78615c8
camille villard <hello@camillevillard.com>
2019-09-06 16:43:27 -0400

    added multiple tags

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -11,11 +11,25 @@ class WinesController < ApplicationController
   def create
     wine_name = params[:wine_name]
     accords = params[:wine_accord]
+    raw_tags = params[:wine_tag]
+
+    # building the array of tags
+    tags = []
+    raw_tags.split(',').each do |tag|
+      tag[-1] == ',' ? tag[-1] = '' : tag = tag
+      if Tag.find_by_name(tag)
+        tags << Tag.find_by_name(tag)
+      else
+        tags << Tag.create!(name: tag)
+      end
+    end
+    # creating the new wine
     @wine = Wine.new(name: wine_name, accords: accords, description: "", link: "#" )
 
     if @wine.save
-      tag = Tag.find_by_name(params[:wine_tag])
-      @tag_wine = TagWine.create!(wine: @wine, tag: tag)
+      tags.each do |tag|
+        @tag_wine = TagWine.create!(wine: @wine, tag: tag)
+      end
       user = User.find_by_name(params[:wine_user])
       @user_wine = UserWine.create!(wine: @wine, user: user)
       redirect_to root_path
